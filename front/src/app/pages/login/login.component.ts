5091b72cc554c753fa4260804e603db3954dff1b
malikdupuis <maliklegenie@gmail.com>
2024-11-11 22:00:44 +0100

    feat: login and register

--- front/src/app/pages/login/login.component.ts
+++ front/src/app/pages/login/login.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoginRequest } from 'src/app/interfaces/loginRequest.interface';
+import { SessionInformation } from 'src/app/interfaces/sessionInformation.interface';
+import { AuthService } from 'src/app/services/auth.service';
+import { SessionService } from 'src/app/services/session.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +12,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  public onError = false;
 
-  constructor() { }
+  public form = this.fb.group({
+    email: [
+      '',
+      [
+        Validators.required,
+        Validators.email
+      ]
+    ],
+    password: [
+      '',
+      [
+        Validators.required,
+        Validators.min(3)
+      ]
+    ]
+  });
+  constructor(private authService: AuthService,
+    private fb: FormBuilder,
+    private router: Router,
+    private sessionService: SessionService) {
+}
 
   ngOnInit(): void {
+  }
+
+  public submit(): void {
+    const loginRequest = this.form.value as LoginRequest;
+    this.authService.login(loginRequest).subscribe({
+      next: (response: SessionInformation) => {
+        this.sessionService.logIn(response);
+        this.router.navigate(['/']);
+      },
+      error: error => this.onError = true,
+    });
   }
 
 }
--- front/src/app/pages/register/register.component.ts
+++ front/src/app/pages/register/register.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { RegisterRequest } from 'src/app/interfaces/registerRequest.interface';
+import { SessionInformation } from 'src/app/interfaces/sessionInformation.interface';
 import { AuthService } from 'src/app/services/auth.service';
+import { SessionService } from 'src/app/services/session.service';
 
 @Component({
   selector: 'app-register',
@@ -42,7 +44,8 @@ export class RegisterComponent implements OnInit {
 
   constructor(private authService: AuthService,
               private fb: FormBuilder,
-              private router: Router) {
+              private router: Router,
+              private sessionService: SessionService) {
   }
   ngOnInit(): void {
     
@@ -51,8 +54,11 @@ export class RegisterComponent implements OnInit {
   public submit(): void {
     const registerRequest = this.form.value as RegisterRequest;
     this.authService.register(registerRequest).subscribe({
-        next: (_: void) => this.router.navigate(['/login']),
-        error: _ => this.onError = true,
+      next: (response: SessionInformation) => {
+        this.sessionService.logIn(response);
+        this.router.navigate(['/']);
+      },
+      error: error => this.onError = true,
       }
     );
   }
