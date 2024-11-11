c5f60375fe27e1cdd4df08a2be9e44cd11a3e545
malikdupuis <maliklegenie@gmail.com>
2024-11-11 11:04:39 +0100

    login

---
+++ back/src/main/java/com/openclassrooms/mddapi/configuration/SpringSecurityConfig.java
@@ -0,0 +1,4 @@
+package com.openclassrooms.mddapi.configuration;
+
+public class SpringSecurityConfig {
+}
---
+++ back/src/main/java/com/openclassrooms/mddapi/services/JWTService.java
@@ -0,0 +1,54 @@
+package com.example.demo.service;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.*;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class JWTService {
+
+
+    private JwtEncoder jwtEncoder;
+    private JwtDecoder jwtDecoder;
+
+    public JWTService(JwtEncoder jwtEncoder, JwtDecoder jwtDecoder) {
+        this.jwtEncoder = jwtEncoder;
+        this.jwtDecoder = jwtDecoder;
+    }
+
+    public String generateToken(String email) {
+        Instant now = Instant.now();
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .issuer("self")
+                .issuedAt(now)
+                .expiresAt(now.plus(1, ChronoUnit.DAYS))
+                .subject(email)
+                .build();
+        JwtEncoderParameters jwtEncoderParameters = JwtEncoderParameters.from(JwsHeader.with(MacAlgorithm.HS256).build(), claims);
+        return this.jwtEncoder.encode(jwtEncoderParameters).getTokenValue();
+    }
+
+
+
+
+
+    // Extraire l'email du token
+    public String extractEmailFromToken(String token) {
+        Jwt decodedJwt = jwtDecoder.decode(token);
+        return decodedJwt.getSubject(); // Le 'subject' contient l'email que tu as mis dans le token
+    }
+
+    // Valider le token (tu peux ajouter plus de logiques de validation si nécessaire)
+    public boolean validateToken(String token) {
+        try {
+            jwtDecoder.decode(token);
+            return true;
+        } catch (JwtException e) {
+            return false;
+        }
+    }
+}
